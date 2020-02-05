bb6ff0502440a5d28cc208908a9ff2f3fb7a00e2
Karl Oskar Ekvall <k.o.ekvall@gmail.com>
2020-02-05 12:12:41 +0100

    Update fit.R

--- R/fit.R
+++ R/fit.R
@@ -1,20 +1,21 @@
 #' Fit principal components regression using a jointly normal likelihood
 #'
-#' @param Y An n x r matrix of responses.
-#' @param X An n x p matrix of predictors
+#' @param Y n x r matrix of responses
+#' @param X n x p matrix of predictors
 #' @param k Number of principal components to use
 #' @param rho Ridge penalty on alpha in representation beta = L alpha
-#' @param tol The tolerance for L-BFGS-G algorithm used on profile likelihood
-#' @param maxit The maximum number of iterations of L-BFGS-G algorithm
+#' @param tol Tolerance for L-BFGS-G on profile likelihood
+#' @param maxit Maximum number of iterations of L-BFGS-G algorithm
 #' @param center If TRUE, responses and predictors are centered by their sample mean
-#' @param quiet If FALSE, print print information from L-BFGS-G algorithm
-#' @param L Starting value for the Cholesky root in the decomposition
-#'          Sigma_X = tau (I_p + LL^T)
-#' @return List with estimates of beta (regression coefficient),
-#'         Sigma (response covariance matrix),
-#'         Sigma_X (predictor covariance matrix),
-#'         L (Cholesky root in decomposition of Sigma_X), and 
-#'         tau (smallest eigenvalue of Sigma_X; has multiplicity p - k)
+#' @param quiet If FALSE, print information from L-BFGS-G algorithm
+#' @param L Starting value in L-BFGS-B for the Cholesky root
+#'          in the decomposition Sigma_X = tau (I_p + LL^T)
+#' @return List with estimates: 
+#'          beta (regression coefficient)
+#'          Sigma (response covariance matrix)
+#'          Sigma_X (predictor covariance matrix)
+#'          L (Cholesky root in decomposition of Sigma_X)
+#'          tau (smallest eigenvalue of Sigma_X; has multiplicity p - k)
 #' @useDynLib jlpcr, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #' @importFrom Rcpp evalCpp
@@ -44,7 +45,7 @@ jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE, qui
     tau <- mean(e_S$values[(k + 1):p])
     D <- e_S$values[1:k] / tau - 1
     L <- chol_spsd(U %*% diag(D, k) %*% t(U) ,tol = 1e-8)$L
-    if(ncol(L) > k) warning("PPCA starting calue has more than k columns; dropping.")
+    if(ncol(L) > k) warning("PPCA starting value has more than k columns; dropping.")
     L <- L[, 1:k, drop = F]
     rm(e_S, U, D, tau)
   }
