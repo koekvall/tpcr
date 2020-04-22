1966333ca5372d2aea10d7c9693d0e8dbe943daf
koekvall <k.o.ekvall@gmail.com>
2020-04-22 07:46:14 +0200

    Many updates

--- R/fit.R
+++ R/fit.R
@@ -11,34 +11,46 @@
 #' This covariance matrix is assumed to be spiked, meaning its
 #' smallest eigenvalue has multiplicity p - k.
 #'
-#' @param Y n x r matrix of responses
-#' @param X n x p matrix of predictors
-#' @param k Number of principal components to use
-#' @param rho Ridge penalty on alpha in representation beta = L alpha
-#' @param tol Tolerance for L-BFGS-B on profile likelihood
-#' @param maxit Maximum number of iterations of L-BFGS-B algorithm
-#' @param center If TRUE, responses and predictors are centered by their sample mean
-#' @param scale If TRUE, divide each column in X by its sample standard deviation
-#' @param quiet If FALSE, print information from L-BFGS-B algorithm
-#' @param L Starting value in L-BFGS-B for the Cholesky root
+#' @param Y Matrix (n x r) of responses; rows correspond to observations
+#' @param X Matrix (n x p) of predictors; rows correspond to observations
+#' @param k Integer number of principal components to use; can be a vector
+#' @param rho Numeric ridge penalty on alpha in representation beta = L alpha
+#' @param tol Numeric tolerance for L-BFGS-B on profile log-likelihood
+#' @param maxit Integer maximum number of iterations of L-BFGS-B algorithm
+#' @param mean_Y Bool indicating the mean of Y should be a parameter; if FALSE,
+#'               it is assumed E(Y) = 0.
+#' @param mean_X Bool indicating the mean of X should be a parameter; if FALSE,
+#'               it is assumed E(X) = 0.           
+#' @param scale Bool indicating each predictor is divided by its sample
+#'              standard deviation
+#' @param quiet Bool indicating no information printed from L-BFGS-B algorithm
+#' @param L Matrix (p x k) starting value in L-BFGS-B for the Cholesky root
 #'          in the decomposition Sigma_X = tau (I + LL')
-#' @param m Penalty in information criterion
+#' @param m Numeric penalty in information criterion
 #'          - 2 * log-likelihood + m * n_params; can be a vector
-#' @return List with estimates:
+#' @return If length(k) = 1, returns a list with estimates:
 #'          beta (regression coefficient),
 #'          Sigma (response covariance matrix),
 #'          Sigma_X (predictor covariance matrix),
 #'          L (Cholesky root in decomposition Sigma_X = tau[I + LL']),
 #'          tau (smallest eigenvalue of Sigma_X; has multiplicity p - k), and
-#'          IC (information criterion)
+#'          IC (information criterion).
+#'          
+#'         If length(k) > 1, returns a list of lists of length k + 1 where for
+#'         j in 1:k the jth element is the list returned by jlpcr with k = k[j]
+#'         and the (k + 1)th element is a vector of the same length as m where
+#'         the jth element is the k selected by the IC corresponding to m[j].
 #' @useDynLib jlpcr, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #' @importFrom Rcpp evalCpp
 #' @export
-jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE,
-  scale = FALSE, quiet = TRUE, L, m = 2)
+jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, mean_Y = TRUE,
+  mean_X = TRUE, scale = FALSE, quiet = TRUE, L, m = 2)
 {
+  Y <- as.matrix(Y)
+  X <- as.matrix(X)
   # Define constants
+  k <- sort(k)
   p <- ncol(X)
   n <- nrow(X)
   r <- ncol(Y)
@@ -46,13 +58,21 @@ jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE,
   if(n < p & rho == 0){
     warning("The objective function is unbounded when n < p and rho = 0; do not expect reliable estimates!")
   }
-
-  if(center){
-    X <- scale(X, scale = F)
+  
+  mu_Y <- rep(0, ncol(Y))
+  mu_X <- rep(0, ncol(X))
+  if(mean_Y){
     Y <- scale(Y, scale = F)
+    mu_Y <- attr(Y, "scaled:center")
+  }
+  if(mean_X){
+    X <- scale(X, scale = F)
+    mu_X <- attr(X, "scaled:center")
   }
   
-  if(k[1] >= min(n, p)) stop("jlpcr requires k < min(n, p)")
+  if(scale) X <- scale(X, center = F)
+  
+  if(max(k) >= min(n, p)) stop("jlpcr requires k < min(n, p)")
   if(missing(L)){
     # Use probabilistic principal components estimates
     e_S <- eigen(crossprod(X) / n, symmetric = TRUE)
@@ -61,7 +81,6 @@ jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE,
     tau <- mean(e_S$values[(k[1] + 1):p])
     D <- e_S$values[1:k[1]] / tau - 1
     L <- chol_spsd(U %*% diag(D, k[1]) %*% t(U) , tol = 1e-8)$L
-    if(ncol(L) > k[1]) warning("PPCA starting value has more than k columns; dropping.")
     L <- L[, 1:k[1], drop = F]
     rm(D, tau, U)
   }
@@ -77,11 +96,12 @@ jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE,
     out_list <- list()
     ii <- 1
     for(k_i in k){
-      out_list[[ii]] <- jlpcr(Y = Y, X = X, k = k_i, rho = rho, tol = tol, maxit = maxit,
-            center = center, scale = scale, quiet = quiet, L = L, m = m)
- 
+      out_list[[ii]] <- jlpcr(Y = Y, X = X, k = k_i, rho = rho, tol = tol,
+                              maxit = maxit, mean_Y = mean_Y, mean_X = mean_X,
+                              scale = scale, quiet = quiet, L = L, m = m)
+      
       # New starting value; use directions of first sample evecs not in span
-      if(ii == length(k)) break
+      if(ii == length(k)) break #  No starting values for next iter
       s_L <- svd(out_list[[ii]]$L)
       u_new <- e_S$vectors[, 1:(k[ii + 1] - k_i), drop = F] - 
         s_L$u %*% crossprod(s_L$u, e_S$vectors[, 1:(k[ii + 1] - k_i), drop = F])
@@ -91,7 +111,8 @@ jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE,
                        t(U_hat), tol = 1e-8)$L[, 1:k[ii + 1]]
       ii <- ii + 1
     }
-    out_list[[ii + 1]] <- k[which.min(unlist(lapply(out_list, function(x)x$IC[1])))]
+    all_IC <- do.call(rbind, lapply(out_list, function(x)x$IC))
+    out_list[[ii + 1]] <- k[as.vector(apply(all_IC, 2, which.min))]
     names(out_list) <- c(paste0("fit_k_", k), "k_star")
     return(out_list)
   }
@@ -114,13 +135,16 @@ jlpcr <- function(Y, X, k, rho = 0, tol = 1e-10, maxit = 1e3, center = TRUE,
   n_param <- r * (r + 1) / 2 # Cond covmat
   n_param <- n_param + k * r # alpha
   n_param <- n_param + 1 # tau
-  n_param <- n_param + 2 * p * k - k^2 - (p - k - 1) 
-  
-  IC<-  sum(mvtnorm::dmvnorm(Y -  X %*% beta, sigma = Sigma, log = T))
+  n_param <- n_param + k + k * p - k * (k + 1) / 2 #SPSD rank k
+  if(mean_Y) n_param <- n_param + r # Intercept
+  if(mean_X) n_param <- n_param + p # Predictor mean
+  # k eigenvalues, k orthonormal vectors requires (p - 1) + (p - 2) ... (p - k)
+  # = k + kp - sum_i^k i = k + kp - k(k + 1) / 2
+  IC <-  sum(mvtnorm::dmvnorm(Y -  X %*% beta, sigma = Sigma, log = T))
   IC <- IC + sum(mvtnorm::dmvnorm(X, sigma = Sigma_X, log = T))
   IC <- -2 * IC + m * n_param
   
-  return(list(beta = beta,
+  return(list(mu_Y = mu_Y, mu_X = mu_X, beta = beta,
               Sigma = Sigma, Sigma_X = tau * (diag(1, p) + tcrossprod(L)),
               L = L, tau = tau, IC = IC))
 }
